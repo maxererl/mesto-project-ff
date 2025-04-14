0ab15e2de6422563d4d362aff1272207fefc37d2
maxererl <maxererl@yandex.ru>
2025-04-15 01:23:14 +0300

    final 1.1

--- scripts/index.js
+++ scripts/index.js
@@ -5,16 +5,18 @@ const template = document.querySelector('#card-template').content;
 const cards = document.querySelector('.places__list');
 
 // Функция создания карточки
-function createCard(title, img, removeCard) {
+function createCard(cardData, removeCard) {
   const card = template.querySelector('.card').cloneNode(true);
-  card.querySelector('.card__image').src = img;
-  card.querySelector('.card__description .card__title').textContent = title;
+  const cardImage = card.querySelector('.card__image');
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  card.querySelector('.card__description .card__title').textContent = cardData.name;
   card.querySelector('.card__delete-button').addEventListener('click', removeCard);
   return card;
 }
 
 // Функция удаления карточки
-removeCard = (evt) => evt.target.closest('.card').remove();
+const removeCard = (evt) => evt.target.closest('.card').remove();
 
 // Вывести карточки на страницу
-initialCards.forEach(card => cards.append(createCard(card.name, card.link, removeCard)));
+initialCards.forEach(card => cards.append(createCard(card, removeCard)));

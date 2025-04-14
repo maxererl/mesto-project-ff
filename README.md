ba8e531ad052e488956f04782578a80103088bed
maxererl <maxererl@yandex.ru>
2025-04-15 00:05:55 +0300

    final 1.0

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+https://github.com/maxererl/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,20 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
+const template = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+// DOM узлы
+const cards = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+// Функция создания карточки
+function createCard(title, img, removeCard) {
+  const card = template.querySelector('.card').cloneNode(true);
+  card.querySelector('.card__image').src = img;
+  card.querySelector('.card__description .card__title').textContent = title;
+  card.querySelector('.card__delete-button').addEventListener('click', removeCard);
+  return card;
+}
 
-// @todo: Функция удаления карточки
+// Функция удаления карточки
+removeCard = (evt) => evt.target.closest('.card').remove();
 
-// @todo: Вывести карточки на страницу
+// Вывести карточки на страницу
+initialCards.forEach(card => cards.append(createCard(card.name, card.link, removeCard)));
